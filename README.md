70ff31e04a857af63de2e590f43195c8c4ca9a7a
Emanuel Sidon <emanuel-ferrera@hotmail.com>
2021-11-09 20:41:08 -0300

    checkpoint Programaçao imperativa

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Checkpoint da matéria de Programação Imperatica<h1>
+<hr>
+<p>Aqui está sendo colocado minha aprendizagem referente a máteria de programação 
+imperativa para avaliação do meu desenvolvimento pessoal até o momento.<p>
---
+++ checkpoint1.js
@@ -0,0 +1,87 @@
+let pipoca = 10;
+let macarrao = 8;
+let carne = 15;
+let feijao = 12;
+let brigadeiro = 8;
+
+/*const tempoPreparo = (pipoca) => {
+  if (pipoca < 10) {
+    console.log("Comida sem tempo de preparo suficiente");
+  }
+  else if (pipoca == 10 || pipoca <=20) {
+    console.log("Prato pronto, bom apetite!!!");
+  }
+  else if (pipoca > 20 && pipoca <= 30) {
+    console.log("Sua comida queimou!!!")
+  }
+  else {
+    console.log("KABUUUMMMM");
+  }
+}
+console.log(tempoPreparo(31)); */
+
+/*
+const tempoPreparo = (macarrao) => {
+  if (macarrao < 8) {
+    console.log("Comida sem tempo de preparo suficiente");
+  }
+  else if (macarrao == 8 || macarrao <=16) {
+    console.log("Prato pronto, bom apetite!!!");
+  }
+  else if (macarrao > 16 && macarrao <= 24) {
+    console.log("Sua comida queimou!!!")
+  }
+  else {
+    console.log("KABUUUMMMM");
+  }
+}
+console.log(tempoPreparo(25)); */
+
+
+/* const tempoPreparo = (carne) => {
+  if (carne < 15) {
+    console.log("Comida sem tempo de preparo suficiente");
+  }
+  else if (carne == 15 || carne <=30) {
+    console.log("Prato pronto, bom apetite!!!");
+  }
+  else if (carne > 30 && carne <= 45) {
+    console.log("Sua comida queimou!!!")
+  }
+  else {
+    console.log("KABUUUMMMM");
+  }
+}
+console.log(tempoPreparo(47)); */
+
+/* const tempoPreparo = (feijao) => {
+  if (feijao < 12) {
+    console.log("Comida sem tempo de preparo suficiente");
+  }
+  else if (feijao == 12 || feijao <=24) {
+    console.log("Prato pronto, bom apetite!!!");
+  }
+  else if (feijao > 24 && feijao <= 36) {
+    console.log("Sua comida queimou!!!")
+  }
+  else {
+    console.log("KABUUUMMMM");
+  }
+}
+console.log(tempoPreparo(37)); */
+
+const tempoPreparo = (brigadeiro) => {
+  if (brigadeiro < 8) {
+    console.log("Comida sem tempo de preparo suficiente");
+  }
+  else if (brigadeiro == 8 || brigadeiro <=16) {
+    console.log("Prato pronto, bom apetite!!!");
+  }
+  else if (brigadeiro > 16 && brigadeiro <= 24) {
+    console.log("Sua comida queimou!!!")
+  }
+  else {
+    console.log("KABUUUMMMM");
+  }
+}
+console.log(tempoPreparo(25));
